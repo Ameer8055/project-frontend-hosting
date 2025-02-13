16ead90eee741fa3bef9b366406e32aa9fc5050f
Ameer8055 <ameerin1947@gmail.com>
2025-02-13 16:16:35 +0530

    background color

--- src/components/navbars/NavbarViewer.jsx
+++ src/components/navbars/NavbarViewer.jsx
@@ -78,8 +78,7 @@ const NavbarViewer = ({ onGenreChange, onSearch }) => {
         style={{
           backgroundSize: "cover",
           backgroundPosition: "center",
-          backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent overlay
-          backdropFilter: "blur(5px)", // Adds a blur effect
+          backgroundColor: "transparent"
         }}
       >
         <Toolbar>
